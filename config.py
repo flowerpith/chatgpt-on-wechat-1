052d6d35d3a0c49e8d22806ca53a53a4c86325cd
flowerpith <82866969+flowerpith@users.noreply.github.com>
2023-04-20 11:56:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ from common.log import logger
 # 将所有可用的配置项写在字典里, 请使用小写字母
 available_setting = {
     # openai api配置
-    "open_ai_api_key": "",  # openai api key
+    "open_ai_api_key": "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",  # openai api key
     # openai apibase，当use_azure_chatgpt为true时，需要设置对应的api base
     "open_ai_api_base": "https://api.openai.com/v1",
-    "proxy": "",  # openai使用的代理
+    "proxy": "127.0.0.1:7890",  # openai使用的代理
     # chatgpt模型， 当use_azure_chatgpt为true时，其名称为Azure上model deployment名称
     "model": "gpt-3.5-turbo",
     "use_azure_chatgpt": False,  # 是否使用azure的chatgpt
